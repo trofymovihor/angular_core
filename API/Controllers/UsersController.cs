a20058882c5c4e89721482f34980b6201ac540e6
trofymovihor <ihortrofymov@gmail.com>
2024-11-15 15:44:51 +0200

    Section 4 - API Identity

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,24 +1,28 @@
 using System;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class UsersController(DataContext context) : ControllerBase
-{
 
+public class UsersController(DataContext context) : BaseApiController
+{
+    [AllowAnonymous]
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() {
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
         var users = await context.Users.ToListAsync();
         return Ok(users);
     }
 
+
+    [Authorize]
     [HttpGet("{id}")]
-    public async Task<ActionResult<AppUser>> GetUser(int id) {
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
         var user = await context.Users.FindAsync(id);
 
         if (user == null) return NotFound();
--- API/Program.cs
+++ API/Program.cs
@@ -1,17 +1,14 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using System.Text;
+using API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<DataContext>(opt =>{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 app.UseCors(x =>{
@@ -19,6 +16,9 @@ app.UseCors(x =>{
     .AllowAnyMethod()
     .WithOrigins("http://localhost:4200", "https://localhost:4200");
 });
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
