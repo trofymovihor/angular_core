404db658cf12cd9356f0e07b0fbfbb903d6773c8
trofymovihor <ihortrofymov@gmail.com>
2024-11-23 01:44:39 +0200

    section 6 routing

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -11,6 +12,7 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent {
   private accountService = inject(AccountService);
+  private toastrService = inject(ToastrService);
 model:any = {}
 cancelRegister = output<boolean>()
 register() {
@@ -19,9 +21,7 @@ register() {
       console.log(response);
       this.cancel();
     },
-    error: error =>{
-      console.log(error)
-    }
+    error: error => this.toastrService.error(error.error)
   })
 }
 
