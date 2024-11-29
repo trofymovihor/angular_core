a93ed31f46f4163e9caffe7cae4c0bfabd50bb2e
trofymovihor <ihortrofymov@gmail.com>
2024-11-29 02:23:24 +0200

    section 10 updating resourses

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,5 @@
+using System.Security.Claims;
+using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
@@ -9,7 +11,7 @@ namespace API.Controllers;
 
 
 [Authorize]
-public class UsersController(IUserRepository repository) : BaseApiController
+public class UsersController(IUserRepository repository, IMapper mapper) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
@@ -26,5 +28,15 @@ public class UsersController(IUserRepository repository) : BaseApiController
         if (user == null) return NotFound();
 
         return user;
+    }
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberDto){
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (username == null) return BadRequest("No username found in Token");
+        var user = await repository.GetUserByUsernameAsync(username);
+        if (user ==null) return BadRequest("User not found");
+        mapper.Map(memberDto, user);
+        if (await repository.SaveAllAsync()) return NoContent();
+        return BadRequest("Failed to update user");
     }
 }
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -3,12 +3,11 @@ import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [TabsModule, GalleryModule],
+  imports: [TabsModule],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css',
   
@@ -20,17 +19,16 @@ export class MemberDetailComponent implements OnInit {
   private memberService = inject(MembersService);
   private route =inject(ActivatedRoute);
   member?: Member
-  images: GalleryItem[] = [];
 
   loadMember() {
     const username = this.route.snapshot.paramMap.get('username');
     if (!username) return;
     this.memberService.getMember(username).subscribe({
       next: member => {
-        this.member = member
-        member.photos.map(p=> {
-          this.images.push(new ImageItem({ src: p.url, thumb: p.url}))
-        })
+        this.member = member;
+        // member.photos.map(p=> {
+        //   this.images.push(new ImageItem({ src: p.url, thumb: p.url}))
+        // })
       }
     })
   }
